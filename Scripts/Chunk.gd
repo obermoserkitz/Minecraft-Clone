d7ae284d3a1220fa391e68f2d0c91bf1619f5e7d
Your Name <you@example.com>
2022-10-25 08:31:46 +0200

    Mesh Generation

--- Scripts/Chunk.gd
+++ Scripts/Chunk.gd
@@ -13,7 +13,7 @@ const vertices = [
 ]
 
 const TOP = [2,3,7,6]
-const BOTTOM = [0,2,5,1]
+const BOTTOM = [0,4,5,1]
 const LEFT=[6,4,0,2]
 const RIGHT=[3,1,5,7]
 const FRONT=[7,5,4,6]
@@ -25,6 +25,8 @@ var st = SurfaceTool.new()
 var mesh = null
 var mesh_instance = null
 
+func _ready():
+	update()
 
 func update():
 	if mesh_instance !=null:
@@ -39,3 +41,26 @@ func update():
 		for y in Global.DIMENSION.y:
 			for z in Global.DIMENSION.z:
 				create_block(x,y,z)
+
+	st.generate_normals(false)
+	st.commit(mesh)
+	mesh_instance.set_mesh(mesh)
+	add_child(mesh_instance)
+	mesh_instance.create_trimesh_collision()
+	
+func create_block(x,y,z):
+	create_face(TOP, x,y,z)
+	create_face(BOTTOM, x,y,z)
+	create_face(LEFT, x,y,z)
+	create_face(RIGHT, x,y,z)
+	create_face(BACK, x,y,z)
+	create_face(FRONT, x,y,z)
+
+func create_face(i, x,y,z):
+	var offset = Vector3(x,y,z)
+	var a = vertices[i[0]] + offset
+	var b = vertices[i[1]] + offset
+	var c = vertices[i[2]] + offset
+	var d = vertices[i[3]] + offset
+	st.add_triangle_fan(([[a,b,c]]))
+	st.add_triangle_fan(([a,c,d]))
